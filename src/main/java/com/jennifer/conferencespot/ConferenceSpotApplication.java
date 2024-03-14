29b1358b950329ce90425e0fdf75ef53974c896e
Jennifer-I <jenniferewuzie27@gmail.com>
2024-03-14 04:43:49 +0100

    add github actions workflow to build and test pull requests

---
+++ src/main/java/com/jennifer/conferencespot/ConferenceSpotApplication.java
@@ -0,0 +1,13 @@
+package com.jennifer.conferencespot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceSpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jennifer/conferencespot/ConferenceSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jennifer.conferencespot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
